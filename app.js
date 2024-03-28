48be918b9fa5e03b007c1efdc86f8a8239235f3e
Vinit <vinitsk99@gmail.com>
2024-03-28 22:51:28 +0530

    add features

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("show");
-  }
+// function toggleMenu() {
+//     var menu = document.getElementById("menu");
+//     menu.classList.toggle("show");
+//   }
 
-  function logout() {
-    // Add logout functionality here
-    alert("Logging out...");
+//   function logout() {
+//     alert("Logging out...");
+//   }
+
+
+// ----Login Toggle----
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+}
+
+
+// ----AboutUs Slides----
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
+
+showSlides();
+
+
+//Fixed Nav Bar
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  if (window.scrollY > 0) {
+  navbar.classList.add('navbar-scrolled');
+  } else {
+  navbar.classList.remove('navbar-scrolled');
+  }
+});
